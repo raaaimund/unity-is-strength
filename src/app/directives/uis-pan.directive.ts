452bc7b05ff43a10d79a3b0a061e3f7af2d2467e
Raimund Rittnauer <raimund.rittnauer@rise-world.com>
2024-03-09 18:37:37 +0100

    use bottom sheet container

--- src/app/directives/uis-pan.directive.ts
+++ src/app/directives/uis-pan.directive.ts
@@ -13,25 +13,25 @@ export class UisPanDirective {
   @Output() uisPanEnded = new EventEmitter<UisPanEndedEvent>();
 
   @HostListener("panstart", ["$event"]) onPanStart(event: HammerInput) {
-    const container = this.el.nativeElement.parentElement;
+    const container = this.el.nativeElement.parentElement?.closest('.plant-bottom-sheet') as HTMLElement | null
     if (!container) throw new Error('No container found');
-    container.style.position = 'relative';
+    container.style.position = 'absolute';
   }
 
   @HostListener("panup", ["$event"]) onPanUp(event: HammerInput) {
-    const container = this.el.nativeElement.parentElement;
+    const container = this.el.nativeElement.parentElement?.closest('.plant-bottom-sheet') as HTMLElement | null
     if (!container) throw new Error('No container found');
-    container.style.top = `${event.deltaY}px`;
+    container.style.top = `${event.center.y}px`;
   }
 
   @HostListener("pandown", ["$event"]) onPanDown(event: HammerInput) {
-    const container = this.el.nativeElement.parentElement;
+    const container = this.el.nativeElement.parentElement?.closest('.plant-bottom-sheet') as HTMLElement | null
     if (!container) throw new Error('No container found');
-    container.style.top = `${event.deltaY}px`;
+    container.style.top = `${event.center.y}px`;
   }
 
   @HostListener("panend", ["$event"]) onPanEnd(event: HammerInput) {
-    const container = this.el.nativeElement.parentElement;
+    const container = this.el.nativeElement.parentElement?.closest('.plant-bottom-sheet') as HTMLElement | null
     if (!container) throw new Error('No container found');
     this.uisPanEnded.emit({container, panValues: event});
   }
